06a3adbd9c07f93d5e261d4959b50471b7812e5f
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-01-07 10:11:06 +0800

    grid

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var size = 25
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 400);
 }
   
 function draw() {
-    background(220);
+    background(65);
+    for (var x = 0; x < width; x += width / size / 2) {
+		for (var y = 0; y < height; y += height / size) {
+			stroke(45);
+			strokeWeight(.125);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+		}
+	}
 }
