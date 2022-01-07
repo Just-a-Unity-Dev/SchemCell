fca0625849bd695d1417b4df235649ee523dd79d
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-01-07 10:45:05 +0800

    mouse

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ function setup() {
     createCanvas(800, 400);
 }
   
+function roundToN(N){
+    return Math.ceil(N / 10) * 10;
+}
+
 function draw() {
     background(65);
+    console.log(round(mouseX))
+    rect(roundToN(mouseX), roundToN(mouseY), size / 2, size / 2)
     for (var x = 0; x < width; x += width / size / 2) {
 		for (var y = 0; y < height; y += height / size) {
 			stroke(45);
@@ -14,4 +20,8 @@ function draw() {
 			line(0, y, width, y);
 		}
 	}
+
+    fill(255,255,255)
+    text(`${Math.round(frameRate())}`, 2, 15)
+    noCursor()
 }
